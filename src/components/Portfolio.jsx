9223fd6733f831c42b8da2472b3714b733b7440c
kelvinempire <kelvinewurum@gmail.com>
2025-01-27 10:17:31 +0100

    fix: center align heading in Portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -285,7 +285,7 @@ const Portfolio = () => {
   return (
     <main id="portfolio" className="p-4 sm:p-6 md:p-0 lg:p-0">
       <div className="border-b-2">
-        <h1 className="text-zinc-400 flex justify-center font-serif text-lg sm:text-2xl md:text-3xl lg:text-4xl pt-6 sm:pt-8 md:pt-10 lg:pt-12 pb-6 sm:pb-8 md:pb-10 lg:pb-10  font-bold">
+        <h1 className="text-zinc-400 flex justify-center text-center font-serif text-lg sm:text-2xl md:text-3xl lg:text-4xl pt-6 sm:pt-8 md:pt-10 lg:pt-12 pb-6 sm:pb-8 md:pb-10 lg:pb-10  font-bold">
           Discover + 20 projects that I made with    <span className="animate-ping"> ❤️</span>
         </h1>
       </div>
